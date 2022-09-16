892754a7f78c425c5701a77580a16405b5061eca
suhanlim <soohan2009@naver.com>
2022-09-16 21:31:43 +0900

    88. Merge Sorted Array [java]

---
+++ MergeSortedArray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    	int j=0;
+    	for(int i=nums1.length-n;i<nums1.length;i++) {
+    		nums1[i]=nums2[j++];
+    	}
+    	Arrays.sort(nums1);
+    }
+}
+public class MergeSortedArray {
+
+}
